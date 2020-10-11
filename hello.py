55af02d4f8e01ba8dad8a03d47e653907a45bf68
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-11 10:49:08 +0200

    comments

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("\n")
+x = 5
+print(x)
+print("\n")
+#comment
+y = "Bye"
+print(y)
+print("\n")
+#Multi
+#Line
+#Comments
+"""
+Triple
+Quotes
+Multi
+Line
+String
+Comments
+"""
+z = "No comment"
+print(z)
+print("\n")
