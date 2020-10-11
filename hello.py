8b28aa87d283cdfda953601201b58f11f8cb9381
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-11 13:01:46 +0200

    random number

--- hello.py
+++ hello.py
@@ -1,36 +1,7 @@
+import random
+
 print("\n")
 
-x = 2 #int
-y = 3.4 #float
-z = 5j #complex
-
-#convert int to float
-a = float(x)
-
-#convert int to complex
-b = complex(x)
-
-#convert float to int
-c = int(y)
-
-#convert float to complex
-d = complex(y)
-
-#CANNOT CONVERT COMPLEX NUMBERS INTO ANOTHER NUMBER TYPE
-
-
-
-print(type(a))
-print(a)
-print("\n")
-print(type(b))
-print(b)
-print("\n")
-print(type(c))
-print(c)
-print("\n")
-print(type(d))
-print(d)
-print("\n")
+print(random.randrange(1, 10))
 
 print("\n")
