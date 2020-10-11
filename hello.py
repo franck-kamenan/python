4f428f2a85e0eda20451874055e496c25e784b77
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-11 15:00:19 +0200

    strings

--- hello.py
+++ hello.py
@@ -1,15 +1,62 @@
 print("\n")
 
-#casting from a float and int to a string
-x = """Bonjour
- Comment ça va?
- Cordialement"""
+#get character at position 1
+x = "Bonjour, salut!"
+print(x[1])
 
-y = '''Bonjour
- Comment ça va?
- Cordialement'''
+#negative indexing
+print(x[-5])
 
-print(x)
+#slicing
+print(x[3:7])
+
+#negative slicing
+print(x[-4:-1])
+
+#length
+print(len(x))
+
+#strip() method
+print(x.strip())
+
+#lower() method
+print(x.lower())
+
+#upper() method
+print(x.upper())
+
+#replace() method
+print(x.replace("Bon", "Mal"))
+
+#split() method
+print(x.split(","))
+
+#in
+y = "onjo" in x
 print(y)
+
+#not in
+y = "onjo" not in x
+print(y)
+
+#concatenation
+z = "Bye!"
+print(x + " " + z)
+
+#format() method
+age = 36
+txt = "My name is John and I am {} years old."
+print(txt.format(age))
+numberOfCars = 2
+numberOfSeats = 3
+price = 400
+myorder = "I want {} cars with {} seats at {} dollars."
+print(myorder.format(numberOfCars, numberOfSeats, price))
+#with index numbers
+expensive = "{2} dollars is too much for {1} seats."
+print(expensive.format(numberOfCars, numberOfSeats, price))
+
+#escape character
+print("We are the so-called \"Vikings\" from the north.")
 
 print("\n")
