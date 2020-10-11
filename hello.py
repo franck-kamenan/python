a0b9be94e5fc91baf07ea22f5d126f137ec2c01e
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-11 11:46:02 +0200

    setting the specific data type

--- hello.py
+++ hello.py
@@ -1,31 +1,12 @@
 print("\n")
-x = "awesome"
-print(type(x))
-x = 20
-print(type(x))
-x = 20.5
-print(type(x))
-x = 1j
-print(type(x))
-x = ["apple", "Banana"]
-print(type(x))
-x = ("apple", "Banana")
-print(type(x))
-x = range(6)
-print(type(x))
-x = {"name" : "John", "age" : 36}
-print(type(x))
-x = {"apple", "Banana"}
-print(type(x))
-x = frozenset({"apple", "Banana"})
-print(type(x))
-x = True
-print(type(x))
-x = b"Hello"
-print(type(x))
-x = bytearray(5)
-print(type(x))
-x = memoryview(bytes(5))
-print(type(x))
 
+x = str ("awesome")
+print(type(x))
+x = int (20)
+print(type(x))
+x = float (20.5)
+print(type(x))
+x = complex (1j)
+print(type(x))
+#etc...
 print("\n")
